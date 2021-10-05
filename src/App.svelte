d4c4199e6457464296b522f86c05114216c9a981
bmehder <bmehder@gmail.com>
2021-10-05 13:57:29 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,67 @@
+<script>
+  import Note from './Note.svelte'
+
+  let allNotes = []
+
+  function getNotes() {
+    return JSON.parse(localStorage.getItem('stickynotes')) || []
+  }
+
+  function saveNotes() {
+    localStorage.setItem('stickynotes', JSON.stringify(allNotes))
+  }
+
+  function addNote() {
+    allNotes = [
+      ...allNotes,
+      { id: Math.floor(Math.random() * 100000), content: '' },
+    ]
+  }
+
+  function deleteNote(id) {
+    const isDelete = confirm('You want to delete this note, yeah?')
+
+    if (isDelete) {
+      allNotes = allNotes.filter(note => note.id !== id)
+      saveNotes()
+    }
+  }
+
+  const getNotesFromLocalStorage = () => (allNotes = getNotes())
+
+  $: console.log(allNotes)
+</script>
+
+<main use:getNotesFromLocalStorage>
+  {#each allNotes as note}
+    <Note
+      bind:content={note.content}
+      on:blur={saveNotes}
+      on:dblclick={() => deleteNote(note.id)}
+    />
+  {/each}
+  <button on:click={addNote}>+</button>
+</main>
+
+<style>
+  main {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 200px);
+    padding: 1.5rem;
+    gap: 1.5rem;
+  }
+  button {
+    height: 200px;
+    border: none;
+    outline: none;
+    background: rgba(0, 0, 0, 0.1);
+    border-radius: 10px;
+    font-size: 120px;
+    color: rgba(0, 0, 0, 0.5);
+    cursor: pointer;
+    transition: background 0.2s;
+  }
+  button:hover {
+    background: rgba(0, 0, 0, 0.2);
+  }
+</style>
---
+++ src/Note.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let content
+</script>
+
+<textarea bind:value={content} on:blur on:dblclick />
+
+<style>
+  textarea {
+    height: 200px;
+    padding: 1.5rem;
+    border: none;
+    border-radius: 1em;
+    box-shadow: 0 0 7px rgba(0, 0, 0, 0.15);
+    font-family: inherit;
+    font-size: 1rem;
+  }
+</style>
